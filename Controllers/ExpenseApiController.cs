457b6f6f3f6fa5512ef9612de1f6d340b1169479
Shivanshu6555 <shivanshubhargava63@gmail.com>
2026-01-04 01:22:19 +0530

    Added a API controller to create a get endpoint for Expense Model

---
+++ Controllers/ExpenseApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpendSmart.Models;
+
+namespace SpendSmart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseApiController : ControllerBase
+    {
+
+        private readonly ExpenseTrackerDBContext _dbContext;
+        public ExpenseApiController(ExpenseTrackerDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Expense>>> GetPaymentDetails()
+        {
+            return await _dbContext.Expenses.ToListAsync();
+        }
+    }
+}
