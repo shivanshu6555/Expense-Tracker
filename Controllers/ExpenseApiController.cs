793c3546254c932a3f8da3636b5b762d995d688f
Shivanshu6555 <shivanshubhargava63@gmail.com>
2026-01-05 23:03:36 +0530

    Added PUT, POST, DELETE and Get by id endpoint for expense api controller.

--- Controllers/ExpenseApiController.cs
+++ Controllers/ExpenseApiController.cs
@@ -20,5 +20,47 @@ namespace SpendSmart.Controllers
         {
             return await _dbContext.Expenses.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Expense>> GetPaymentDetails(int id)
+        {
+            var exp = _dbContext.Expenses.FindAsync(id);
+            return Ok(exp) ;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Expense>> PostPaymentDetails(Expense expense)
+        {
+            _dbContext.Expenses.Add(expense);
+            await _dbContext.SaveChangesAsync();
+            return Ok(expense);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Expense>> EditPaymentDetails(int id, Expense expense)
+        {
+            var curexpense = await _dbContext.Expenses.FindAsync(id);
+
+            if (curexpense == null)
+                return NotFound();
+
+            curexpense.Value = expense.Value;
+            curexpense.Description = expense.Description;
+            curexpense.Category = expense.Category;
+
+            await _dbContext.SaveChangesAsync();
+            return Ok(curexpense);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemovePaymentDetails(int id)
+        {
+            var curexpense = await _dbContext.Expenses.FindAsync(id);
+             _dbContext.Expenses.Remove(curexpense);
+            await _dbContext.SaveChangesAsync();
+            return Ok("deleted id : " + id);
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ExpenseTrackerDBContext>(ServiceLifetime.Transient);
 builder.Services.AddDbContext<ExpenseTrackerDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 var app = builder.Build();
 
